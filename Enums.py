15950604d01583b85ddf96724e8f4980b31d896a
이윤상 <kwjdduf88@gmail.com>
2024-10-17 14:28:06 +0900

    Enums file

---
+++ Enums.py
@@ -0,0 +1,20 @@
+import enum
+
+class LayerType(enum.Enum):
+    Non = 0
+    BackGround = enum.auto()
+    Tree = enum.auto()
+    Player = enum.auto()
+    Enemy = enum.auto()
+    Supplies = enum.auto()
+    Camera = enum.auto()
+    End = enum.auto()
+    pass
+
+class ComponentType(enum.Enum):
+    Transform = 0
+    Collider = enum.auto()
+    Script = enum.auto()
+    Animation = enum.auto()
+    Camera = enum.auto()
+    End = enum.auto()
